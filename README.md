50d46e3585a7aa3615e097b6a8bae4be2946af92
Tim Pham <timmyhoa34@gmail.com>
2022-08-15 17:56:58 -0400

    Create first post models and add followers to user model

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Social Media
 A full stack social media website made with django and react. 
+
+
+## Extra stuff
+- Associate each like to a user, so instead of just keeping count of the likes, I'm going to keep count of the user who like the post
--- network/models.py
+++ network/models.py
@@ -3,4 +3,28 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    followers = models.ForeignKey(
+        'self', 
+        null= True,
+        blank= True,
+        on_delete= models.DO_NOTHING,
+        related_name='following')
+
+
+class post(models.Model):
+    content = models.CharField(max_length=280)
+    time = models.DateTimeField()
+    user = models.ForeignKey(
+        User, 
+        on_delete= models.SET('Deleted user'),
+        related_name= 'posts'
+        )
+    likes = models.ManyToManyField(
+        User,
+        blank=True, 
+        null= True,
+        related_name='allLikedUser',
+        )
+    
+    def __str__(self) -> str:
+        return f'{self.time}: {self.user}: {self.likes}'
